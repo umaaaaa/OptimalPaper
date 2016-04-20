4d72c53482dbca141f8bccab5542dd46677c8c83
koropicot <koropicot@gmail.com>
2016-04-20 12:13:18 +0000

    Add comment

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var passport = require('../models/passport');
 
+//Auth by Twitter routing
 router.get('/twitter', passport.authenticate('twitter'));
 
 router.get('/twitter/callback', 
@@ -10,6 +11,7 @@ router.get('/twitter/callback',
     { successRedirect: '/',
       failureRedirect: '/login' }));
 
+//Logout
 router.get('/logout', function(req, res, next) {
   req.logout();
   res.redirect('/');
