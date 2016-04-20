b9305ec1f8a58c6dbc2d4f2ff4705f69370a651f
koropicot <koropicot@gmail.com>
2016-04-20 08:00:08 +0000

    models/userでユーザの取得と更新/追加

---
+++ models/users.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var db = require('./db');
+
+var users = {};
+
+users.getByAuthId = function (auth, id_auth) {
+  return new Promise(function(resolve, reject) {
+    db.query('select id,auth,id_auth,name from user where auth = ? and id_auth = ?',
+      [auth, id_auth],
+      function(err, rows) {
+        if (err) return reject(err);
+        if (rows.length == 1) return resolve(rows[0]);
+        reject(new Error('Count of selected user is not 1'));
+      });
+    });
+};
+
+users.updateOrInsert = function (auth, id_auth, name) {
+  return new Promise(function(resolve, reject) {
+    db.query('select id,auth,id_auth,name from user where auth = ? and id_auth = ?',
+      [auth, id_auth],
+      function(err, rows) {
+        if (err) return reject(err);
+        if (rows.length >= 1) {
+          let id = rows[0].id;
+          db.query('update user set ? where id = ?',
+            [{name:name}, id],
+            function (err, res) {
+              if (err) return reject(err);
+              resolve({id:id, auth:auth, id_auth:id_auth, name:name});
+            });
+        }
+        else {
+          db.query('insert into user set ?',
+            {auth:auth, id_auth:id_auth, name:name},
+            function (err, res) {
+              if (err) return reject(err);
+              resolve({id:res.insertId, auth:auth, id_auth:id_auth, name:name});
+            });
+        }
+      });
+    });
+};
+
+module.exports = users;
