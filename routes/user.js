f286db3d9fefd36e977d1b9a465d109f9e4308e3
tsukumaru <came.and.rabbit@gmail.com>
2016-04-21 19:14:04 +0900

    twitterアイコン追加

--- routes/user.js
+++ routes/user.js
@@ -2,25 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var papers = require('../models/papers');
-/* GET users listing. */
+var users = require('../models/users');
+
 /* とりあえずindexのときと同じ形にした */
 router.get('/:id', function(req, res, next) {
-  if(req.user) {
-    //認証あり
-    papers.getOptimal(4, req.user)
-      .then(function(ps) {
-        res.render('user', {id: req.params.id, type: 'optimal', papers: ps, user: req.user});
-      })
-      .catch(next);
-  }
-  else {
-    //認証なし
-    papers.getRecent(4)
-      .then(function(ps) {
-        res.render('user', {id: req.params.id, type: 'recent', papers: ps});
-      })
-      .catch(next);
-  }
+  users.getById(req.params.id)
+    .then(function(us) {
+      console.log("認証ずみ！！！！");
+      res.render('user', {type: 'optimal', users: us});
+    })
+    .catch(next);
 });
 
 module.exports = router;
