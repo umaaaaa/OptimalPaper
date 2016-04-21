e7fc0cff0348a38404828aee1eb72c54d4f39a9c
umaaaaa <k-kurakata@hykwlab.org>
2016-04-21 16:36:21 +0900

    トップページを綺麗にした。( ソースが綺麗とは言っていない )

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var mysql = require('mysql');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var search = require('./routes/search');
 
 var app = express();
 
@@ -37,7 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
