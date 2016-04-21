3f71bc0ccb09583475317f34e4ad8e6d7af97aec
tsukumaru <came.and.rabbit@gmail.com>
2016-04-21 17:21:14 +0900

    ユーザーページに論文要素テンプレートを適用

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var papers = require('../models/papers');
 /* GET users listing. */
-
+/* とりあえずindexのときと同じ形にした */
 router.get('/:id', function(req, res, next) {
-  res.render('user', {'id': req.params.id});
+  if(req.user) {
+    //認証あり
+    papers.getOptimal(4, req.user)
+      .then(function(ps) {
+        res.render('user', {id: req.params.id, type: 'optimal', papers: ps, user: req.user});
+      })
+      .catch(next);
+  }
+  else {
+    //認証なし
+    papers.getRecent(4)
+      .then(function(ps) {
+        res.render('user', {id: req.params.id, type: 'recent', papers: ps});
+      })
+      .catch(next);
+  }
 });
 
 module.exports = router;
