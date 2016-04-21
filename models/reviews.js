7d1c6ef8147826dfa7fce5a28e6ac19fbe982fc8
koropicot <koropicot@gmail.com>
2016-04-21 12:09:14 +0000

    models/reviewsのreview取得部分を修正

--- models/reviews.js
+++ models/reviews.js
@@ -4,15 +4,18 @@ var db = require('./db');
 
 var reviews = {};
 
-reviews.getByPaperId = function (paper_id) {
+reviews.getByPaper = function (paper_id) {
+  console.log(paper_id);
   return new Promise(function(resolve, reject) {
     db.query(
-      'select id,user_id,paper_id,rate,comment,reviewed_at,name,icon_url' +
+      'select review.id,user_id,paper_id,rate,comment,reviewed_at,name,icon_url ' +
       'from review join user on user.id=user_id where paper_id=?',
       [paper_id],
       function(err, rows) {
         if (err) return reject(err);
-        return rows.map(function(row){
+
+        console.log('foo');
+        resolve(rows.map(function(row){
           return {
             id: row.id,
             paper_id: row.paper_id,
@@ -24,8 +27,9 @@ reviews.getByPaperId = function (paper_id) {
               name: row.name,
               icon_url: row.icon_url }
           };
-        });
+        }));
       });
+  });
 };
 
 reviews.getByUserId = function (id) {
