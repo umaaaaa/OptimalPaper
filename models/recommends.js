7b76e4ff508a5af7d5f9207c48403ed34f295c40
koropicot <koropicot@gmail.com>
2016-04-23 09:36:54 +0000

    レーティングのロジック

--- models/recommends.js
+++ models/recommends.js
@@ -2,13 +2,29 @@
 
 var db = require('./db');
 var papers = require('./papers');
+var reviews = require('./reviews');
 
 var recommends = {};
 
+function rating(paper_id) {
+  return reviews.getAvgRateByPaper(id);
+}
+
+function recent(paper_id) {
+  return Promise.reject(new Erro('実装されてない'));
+}
+
+function optimal(paper_id, user) {
+  return Promise.reject(new Error('実装されてない'));
+}
+
 //おすすめ度
-recommends.factor = function (repo, id_repo, orderby, user) {
-  return Promise.resolve(Math.random()*5);
-  
+recommends.factor = function (paper_id, orderby, user) {
+  if (!paper_id) return Promise.resolve(0);
+  if (orderby == 'rating') return rating(paper_id);
+  if (orderby == 'recent') return recent(paper_id);
+  if (orderby == 'optimal') return optimal(paper_id, user);
+  return Promise.reject(new Error());
 }
 
 recommends.getByPaper = function (paper_id, max, user) {
