d7db855b9f4db4bdfa0fc6361e233d3e0f6d9047
tsukumaru <came.and.rabbit@gmail.com>
2016-04-25 09:37:31 +0900

    最終調整

--- public/javascripts/rateyo.js
+++ public/javascripts/rateyo.js
@@ -8,6 +8,6 @@ $(function(){
     $('form .rate').remove();
 
     $('#rateYo').after('<label class="rate"><input type="hidden" name="rate" min=1 max=5 value='+ data.rating +'></label>');
-    $('#rateYo').after('<label class="comment">コメント：<div><textarea  name="comment" placeholder="こめんと"></textarea></div></label>'); 
+    $('#rateYo').after('<label class="comment">コメント：<div><textarea  class="form-control" rows="4" name="comment" style="resize: none;" placeholder="コメントを入力してください"></textarea></div></label>'); 
   })
 });
